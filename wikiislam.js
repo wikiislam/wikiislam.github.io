3356a35e74f33721f7aa819a0b07dbe951a03aae
WikiIslam <wikiislam-unofficial-mirror@wikiislam.net>
2015-12-02 20:49:33 +0100

    Some static files

---
+++ wikiislam.js
@@ -0,0 +1,7 @@
+﻿document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById('searchform').addEventListener('submit', function(e){
+        window.location = 'https://www.google.com/search?q=site:'+ location.host + ' ' + encodeURIComponent(document.getElementById('searchInput').value);
+        return false;
+    });
+});
+
